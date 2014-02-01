fa43593a494d1fbc2aebf3d87bcc325f877b5c6f
Dan Vera <dan@yovu.co>
2014-02-01 09:25:24 -0500

    g

--- index.php
+++ index.php
@@ -25,20 +25,34 @@ use Mailgun\Mailgun;
 		$mgClient = new Mailgun('key-4npkxa6n706uqaxa58ixijv83vdmjwo7');
 		$domain = "sandbox77528.mailgun.org";
 
+		$to = $_POST['from'];
+		$subject = $_POST['stripped-text'];
+		$body = $_POST['content'];
+
+
 
 		if ($_POST['stripped-text'] == 'texto') {
 
-			$pages = 'https://www.readability.com/api/content/v1/parser?url='.$_POST['subject'].'&token=20fe51c16c041aadddf1cd3595cd84701f708c67';
+			$pages = 'https://www.readability.com/api/content/v1/parser?url='.$subject.'&token=20fe51c16c041aadddf1cd3595cd84701f708c67';
 			$pages_json = file_get_contents($pages); 
 			$pages = json_decode($pages_json, TRUE);
 			
 			$result = $mgClient->sendMessage("$domain",
 			  	array('from'    => 'Excited User <bob@sandbox77528.mailgun.org>',
-			  	      'to'      => $_POST['from'],
+			  	      'to'      => $to,
 			  	      'subject' => 'Hello',
 			  	      'text'    => 'hola',
-			  		  'html'    => $pages['content'])
+			  		  'html'    => $body)
 			  	);
+
+		} elseif ($subject == 'imagen') {
+			
+			# code...
+		} elseif ($subject == 'pdf') {
+			
+			# code...
+		} else {
+			// no real request was made so just return text data
 
 		}
 
